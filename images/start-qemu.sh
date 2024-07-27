a04f9099d6bd1dc2e24fda779f5e0e3e597a5bde
daniel <daniel@DRT-Linux>
2024-07-27 15:00:33 -0600

    modifying image

--- images/start-qemu.sh
+++ images/start-qemu.sh
@@ -25,4 +25,4 @@ if ! ${mode_sys_qemu}; then
     export PATH="/home/vagrant/buildroot-2024.02.2/output/host/bin:${PATH}"
 fi
 
-exec qemu-system-arm -M versatilepb -kernel zImage -dtb versatile-pb.dtb -drive file=rootfs.ext2,if=scsi,format=raw -append "rootwait root=/dev/sda console=ttyAMA0,115200"  -net nic,model=rtl8139 -net user  ${EXTRA_ARGS} "$@"
+exec qemu-system-arm -M versatilepb -kernel zImage -dtb versatile-pb.dtb -drive file=rootfs.ext2,if=scsi,format=raw -append "rootwait root=/dev/sda console=ttyAMA0,115200" -net nic,model=rtl8139 -net user,hostfwd=tcp::2222-:22  ${EXTRA_ARGS} "$@"
